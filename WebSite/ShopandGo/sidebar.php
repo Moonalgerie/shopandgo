75c8037c2a9954ae4515d9676b79729193a4b646
Moonalgerie <38092759+Moonalgerie@users.noreply.github.com>
2018-09-10 22:42:41 +0100

    shopandgo

---
+++ WebSite/ShopandGo/sidebar.php
@@ -0,0 +1,62 @@
+<div id="sidebar" class="span3">
+<div class="well well-small">
+	<ul class="nav nav-list">
+		<li><a href="products.php"><span class="icon-chevron-right"></span>Produits Laitiers</a></li>
+		<li><a href="products.php"><span class="icon-chevron-right"></span>Conserves</a></li>
+		<li><a href="products.php"><span class="icon-chevron-right"></span>Produits Frais</a></li>
+		<li><a href="products.php"><span class="icon-chevron-right"></span>Pates</a></li>
+		<li><a href="products.php"><span class="icon-chevron-right"></span>Sucrerie</a></li>
+		<li><a href="products.php"><span class="icon-chevron-right"></span>Légumes Secs</a></li>
+		<li><a href="products.php"><span class="icon-chevron-right"></span>Boissons</a></li>
+		<li><a href="products.php"><span class="icon-chevron-right"></span>Epicerie</a></li>
+		<li><a href="products.php"><span class="icon-chevron-right"></span>Droguerie</a></li>
+		<li><a href="products.php"><span class="icon-chevron-right"></span>Accessoires</a></li>
+		<li style="border:0"> &nbsp;</li>
+		<li> <a class="totalInCart" href="cart.php"><strong>Prix Total <span class="badge badge-warning pull-right" style="line-height:18px;">$448.42</span></strong></a></li>
+	</ul>
+</div>
+
+			  <div class="well well-small alert alert-warning cntr">
+				  <h2>Promotion 50% </h2>
+				  <p>
+					 Sur ces produits <br><br><a class="defaultBtn" href="#">Cliquez ici </a>
+				  </p>
+			  </div>
+			  <div class="well well-small" ><a href="#"><img src="assets/img/livraison.jpg" alt="payment method paypal"></a></div>
+
+			<a class="shopBtn btn-block" href="#">Top produits <br><small>Cliquez ici</small></a>
+			<br>
+			<br>
+			<ul class="nav nav-list promowrapper">
+			<li>
+			  <div class="thumbnail">
+				<a class="zoomTool" href="product_details.php" title="add to cart"><span class="icon-search"></span> Voir plus</a>
+				<img src="assets/img/aa.png" alt="bootstrap ecommerce templates">
+				<div class="caption">
+				  <h4><a class="defaultBtn" href="product_details.php">VIEW</a> <span class="pull-right">$22.00</span></h4>
+				</div>
+			  </div>
+			</li>
+			<li style="border:0"> &nbsp;</li>
+			<li>
+			  <div class="thumbnail">
+				<a class="zoomTool" href="product_details.php" title="add to cart"><span class="icon-search"></span> Voir plus</a>
+				<img src="assets/img/shopping-cart-template.png" alt="shopping cart template">
+				<div class="caption">
+				  <h4><a class="defaultBtn" href="product_details.php">VIEW</a> <span class="pull-right">$22.00</span></h4>
+				</div>
+			  </div>
+			</li>
+			<li style="border:0"> &nbsp;</li>
+			<li>
+			  <div class="thumbnail">
+				<a class="zoomTool" href="product_details.php" title="add to cart"><span class="icon-search"></span> Voir plus</a>
+				<img src="assets/img/bootstrap-template.png" alt="bootstrap template">
+				<div class="caption">
+				  <h4><a class="defaultBtn" href="product_details.php">VIEW</a> <span class="pull-right">$22.00</span></h4>
+				</div>
+			  </div>
+			</li>
+		  </ul>
+
+	</div>

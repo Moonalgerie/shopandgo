bec77dc424fedfaf925c83dd176306a0776170f5
Moonalgerie <38092759+Moonalgerie@users.noreply.github.com>
2018-09-12 19:25:21 +0100

    Update login.php

--- WebSite/ShopandGo/user/login.php
+++ WebSite/ShopandGo/user/login.php
@@ -1,8 +1,9 @@
 <?php
 	if(ISSET ($_POST['login'])){
+		$pseudo = $_POST['pseudo'];
 		$nom_user = $_POST['nom_user'];
 		$mp_user = $_POST['mp_user'];
-		$query = $conn->query("SELECT * FROM `admin` WHERE `nom_user` = '$nom_user' && `mp_user` = '$mp_user'") or die(mysqli_error());
+		$query = $conn->query("SELECT * FROM `user` WHERE `nom_user` = '$nom_user' && `mp_user` = '$mp_user'") or die(mysqli_error());
 		$fetch = $query->fetch_array();
 		$row = $query->num_rows;
 
